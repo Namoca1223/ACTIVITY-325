788b975613ea96020af69193f9963e2f4d0d2e73
Elias-5 <johnpaulcauntoy@gmail.com>
2025-04-27 16:36:02 +0800

    update changes

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ print("Hai Robin")
 #Name Input
 name = str(input("Enter Name: "))
 print("Your Name: ", name)
+
+num = int(input("Enter Number: "))
+print("Number: ", name)
+
+# experiment lang un hehe haha
+# checking experiment
